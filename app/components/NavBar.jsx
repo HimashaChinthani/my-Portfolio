f1fdd50221b257a2a40c56bd9dce627b17301ee7
Himasha Chinthani <140165425+HimashaChinthani@users.noreply.github.com>
2025-10-26 14:33:28 +0530

    create MenuOverlay.jsx

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,6 +1,6 @@
  "use client";
  import React, { useState } from 'react'
-
+import MenuOverlay from "./MenuOverlay"
  import Link from "next/link";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
@@ -27,7 +27,7 @@ const navLinks=[
  const NavBar = () => {
     const[navbarOpen,setNavbarOpen]=useState(false);
    return (
-     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
             <Link href={"/"} className=" text-2xl md:text-5xl text-white font-semibold">LOGO</Link>
             <div className='mobile-menu block md:hidden'>
@@ -58,6 +58,7 @@ const navLinks=[
 </ul>
             </div>
         </div>
+        {navbarOpen ? <MenuOverlay links={navLinks}/> :null}
      </nav>
    )
  }
